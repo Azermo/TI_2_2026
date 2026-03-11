f7974bfb6d200e1c18e60507e1913394adaf0739
Filipe <126357771+Azermo@users.noreply.github.com>
2026-03-11 12:04:29 -0300

    Exercicio 1
    TI 2/2026

---
+++ Soma.java
@@ -0,0 +1,34 @@
+package ti2;
+import java.util.*;
+
+public class Soma {
+	
+	public int d;
+	public int c;
+	
+	
+	public int somar() {
+		
+		return this.d + this.c;
+	}
+
+	public static void main(String[] args) {
+		
+		int resultado = 0;
+		
+		Soma s = new Soma();
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite um numero: ");
+		s.c = scanner.nextInt();
+		System.out.println("Digite outro numero: ");
+		s.d = scanner.nextInt();
+		
+		resultado = s.somar();
+		
+		System.out.println(resultado);
+		
+	}
+
+}
